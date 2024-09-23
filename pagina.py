4d545f834e3acd374d703a3d1cd0e49f62221587
JeffLcTec <j.lopez.5@estudiantec.cr>
2024-09-23 09:34:54 -0600

    Update pagina.py

--- pagina.py
+++ pagina.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
 from datetime import datetime
-st.write("# Roberto Carepicha")
+st.title("# Roberto Carepicha")
 
 st.write("# Bienvenido")
