940d0526996e21786b77ed33c90c7bc53b070568
JeffLcTec <j.lopez.5@estudiantec.cr>
2024-09-24 10:17:43 -0600

    Update 1_Rango_Dia.py

--- pages/1_Rango_Dia.py
+++ pages/1_Rango_Dia.py
@@ -67,7 +67,7 @@ if on:
       yaxis_title="Temperatura (°C)"
    )
    
-   fig1.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['humedad'],mode='lines+markers',line=dict(color='cyan'), name='Humedad'))
+   fig1.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['humedad'],mode='lines+markers',line=dict(color='blue'), name='Humedad'))
    fig1.update_layout(
             title=f"Humedad Promedio desde {fecha_inicio} hasta {fecha_fin}",
             xaxis_title="Fecha",
@@ -121,7 +121,7 @@ else:
       
               # Gráfico de Humedad
            fig_humedad = go.Figure()
-           fig_humedad.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['humedad'], mode='lines', name='Humedad', line=dict(color='cyan')))
+           fig_humedad.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['humedad'], mode='lines', name='Humedad', line=dict(color='blue')))
            fig_humedad.update_layout(
                title=f"Humedad desde {fecha_inicio} hasta {fecha_fin}",
                xaxis_title="Fecha",
