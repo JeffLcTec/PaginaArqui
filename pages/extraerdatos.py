e65f613352fdcfb9a8301b5ccea923c17524f67a
JeffLcTec <j.lopez.5@estudiantec.cr>
2024-09-24 10:20:27 -0600

    Create extraerdatos.py

---
+++ pages/extraerdatos.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+# Reemplaza con tu API_KEY de lectura y el ID del canal
+channel_id = '2665378'
+api_key = '5VXSRHWA713VW8JR'
+
+# URL de la API de ThingSpeak para obtener los datos del campo 1 (por ejemplo)
+url = f'https://api.thingspeak.com/channels/{channel_id}/feeds.json?api_key={api_key}&results=10'
+
+# Realizamos la solicitud HTTP GET
+response = requests.get(url)
+
+# Verificamos si la solicitud fue exitosa
+if response.status_code == 200:
+    # Convertimos la respuesta en formato JSON a un diccionario de Python
+    data = response.json()
+    print(data)
+else:
+    print(f"Error al obtener los datos: {response.status_code}")
