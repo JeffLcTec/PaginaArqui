8f6cfa24a1ae2f10cc2f54be1d8b57e9ef837e63
Roberto <j.chacon.5@estudiantec.cr>
2024-09-24 18:39:05 -0600

    arreglao orta vez

--- pages/1_Rango_Dia.py
+++ pages/1_Rango_Dia.py
@@ -8,7 +8,8 @@ archivo = pd.read_json("temperatura.json")
 datos = pd.json_normalize(archivo['datos'])
 fechas_df = datos[['dia']]
    
-
+   
+   
    # Cuadro de selección para Fecha de Inicio
 fecha_inicio = st.selectbox("Seleccionar Fecha de Inicio", fechas_df)
    
@@ -31,32 +32,33 @@ if on:
     
     
     # Convertir la columna 'dia' a datetime para poder filtrar
-   datos['dia'] = pd.to_datetime(datos['created_at']).dt.date
+   datos['dia'] = pd.to_datetime(datos['dia']).dt.date
     
-    # Convertir la columna 'temperatura' a tipo float
-   datos['field1'] = datos['field1'].astype(float)
+    # Convertir a tipo float
+   datos['temperatura'] = datos['temperatura'].astype(float)
+   datos['humedad'] = datos['humedad'].astype(float)
     
     # Filtrar los datos según el rango de fechas
-   datos_filtrados = datos[(datos['created_at'] >= fecha_inicio) & (datos['created_at'] <= fecha_fin)]
+   datos_filtrados = datos[(datos['dia'] >= fecha_inicio) & (datos['dia'] <= fecha_fin)]
     
     # Calcular el promedio de temperatura en el rango de fechas
-   promedio_temperatura = datos_filtrados['field1'].mean()
-   promedio_humedad = datos_filtrados['field2'].mean()
+   promedio_temperatura = datos_filtrados['temperatura'].mean()
+   promedio_humedad = datos_filtrados['humedad'].mean()
     # Crear la figura del gráfico
    fig = go.Figure()
    fig1 = go.Figure() 
     # Añadir la línea de temperaturas
-   fig.add_trace(go.Scatter(x=datos_filtrados['created_at'], y=datos_filtrados['field1'],mode='lines+markers', line=dict(color='red'),name='Temperatura'))
+   fig.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['temperatura'],mode='lines+markers', line=dict(color='red'),name='Temperatura'))
    fig.update_layout(
             title=f"Temperatura Promedio desde {fecha_inicio} hasta {fecha_fin}",
             xaxis_title="Fecha",
             yaxis_title="Temperatura (°C)",
             height=400,
             xaxis_tickformat='%Y-%m-%d',
-            xaxis=dict(tickmode='array', tickvals=datos_filtrados['created_at'])
+            xaxis=dict(tickmode='array', tickvals=datos_filtrados['dia'])
          ) 
     # Añadir la línea del promedio
-   fig.add_trace(go.Scatter(x=datos_filtrados['created_at'], y=[promedio_temperatura] * len(datos_filtrados),
+   fig.add_trace(go.Scatter(x=datos_filtrados['dia'], y=[promedio_temperatura] * len(datos_filtrados),
                            mode='lines', line=dict(dash='dash', color='cyan'),
                            name=f"Promedio: {promedio_temperatura:.2f} °C"))
     
@@ -67,14 +69,14 @@ if on:
       yaxis_title="Temperatura (°C)"
    )
    
-   fig1.add_trace(go.Scatter(x=datos_filtrados['created_at'], y=datos_filtrados['field2'],mode='lines+markers',line=dict(color='blue'), name='Humedad'))
+   fig1.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['humedad'],mode='lines+markers',line=dict(color='blue'), name='Humedad'))
    fig1.update_layout(
             title=f"Humedad Promedio desde {fecha_inicio} hasta {fecha_fin}",
             xaxis_title="Fecha",
             yaxis_title="Humedad (%)",
             height=400,
             xaxis_tickformat='%Y-%m-%d',
-            xaxis=dict(tickmode='array', tickvals=datos_filtrados['created_at'])
+            xaxis=dict(tickmode='array', tickvals=datos_filtrados['dia'])
          ) 
     # Añadir la línea del promedio
    fig1.add_trace(go.Scatter(x=datos_filtrados['dia'], y=[promedio_humedad] * len(datos_filtrados),
@@ -99,36 +101,36 @@ else:
               
            
               # Convertir la columna 'dia' a datetime para poder filtrar
-           datos['dia'] = pd.to_datetime(datos['created_at']).dt.date
+           datos['dia'] = pd.to_datetime(datos['dia']).dt.date
           
-           datos['field1'] = datos['field1'].astype(float)   
+           datos['temperatura'] = datos['temperatura'].astype(float)   
           
-           datos_filtrados = datos[(datos['created_at'] >= fecha_inicio) & (datos['created_at'] <= fecha_fin)]
+           datos_filtrados = datos[(datos['dia'] >= fecha_inicio) & (datos['dia'] <= fecha_fin)]
           
-           promedio_temperatura = datos_filtrados['field1'].mean() 
+           promedio_temperatura = datos_filtrados['temperatura'].mean() 
              
               # Gráfico de Temperatura
            fig_temp = go.Figure()
-           fig_temp.add_trace(go.Scatter(x=datos_filtrados['created_at'], y=datos_filtrados['field1'], mode='lines', name='Temperatura', line=dict(color='red')))
+           fig_temp.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['temperatura'], mode='lines', name='Temperatura', line=dict(color='red')))
            fig_temp.update_layout(
                title=f"Temperatura desde {fecha_inicio} hasta {fecha_fin}",
                xaxis_title="Fecha",
                yaxis_title="Temperatura (°C)",
                height=350,
                xaxis_tickformat='%Y-%m-%d',
-               xaxis=dict(tickmode='array', tickvals=datos_filtrados['created_at'])
+               xaxis=dict(tickmode='array', tickvals=datos_filtrados['dia'])
            )
       
               # Gráfico de Humedad
            fig_humedad = go.Figure()
-           fig_humedad.add_trace(go.Scatter(x=datos_filtrados['created_at'], y=datos_filtrados['field2'], mode='lines', name='Humedad', line=dict(color='blue')))
+           fig_humedad.add_trace(go.Scatter(x=datos_filtrados['dia'], y=datos_filtrados['humedad'], mode='lines', name='Humedad', line=dict(color='blue')))
            fig_humedad.update_layout(
                title=f"Humedad desde {fecha_inicio} hasta {fecha_fin}",
                xaxis_title="Fecha",
                yaxis_title="Humedad (%)",
                height=350,
                xaxis_tickformat='%Y-%m-%d',
-               xaxis=dict(tickmode='array', tickvals=datos_filtrados['created_at'])
+               xaxis=dict(tickmode='array', tickvals=datos_filtrados['dia'])
            )
       
               # Mostrar los gráficos en Streamlit
