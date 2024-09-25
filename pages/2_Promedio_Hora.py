8d5872053fce7b23fcd78677c97c9c4ac2985c2c
Jeff Lopez <j.lopez.5@estudiantec.cr>
2024-09-24 20:47:58 -0600

    Ver Promedio Hora

--- pages/2_Promedio_Hora.py
+++ pages/2_Promedio_Hora.py
@@ -1,3 +1,125 @@
 import streamlit as st
-
+import pandas as pd
+import plotly.graph_objects as go
+from datetime import datetime
+import json
 st.write('# Promedio por dia')
+archivo = pd.read_json("temperatura.json")
+datos = pd.json_normalize(archivo['datos'])
+fechas_df = datos[['dia']]
+   
+   
+   
+   # Cuadro de selección para Fecha de Inicio
+fecha_seleccionada = st.selectbox("Seleccionar Fecha ", fechas_df)
+   
+   # Cuadro de selección para Fecha de Fin
+
+
+col1,col2,col3 = st.columns([2, 3, 1]) 
+with col3:
+   if st.button("Regresar"):
+      st.switch_page("Inicio.py")
+   
+with col1:
+   on = st.toggle("Ver Promedio")
+
+if on:
+
+    # Convertir las fechas seleccionadas a formato datetime
+   fecha_seleccionada = pd.to_datetime(fecha_seleccionada).date()
+ 
+    
+    
+    # Convertir la columna 'dia' a datetime para poder filtrar
+   datos['dia'] = pd.to_datetime(datos['dia']).dt.date
+    
+    # Convertir a tipo float
+   datos['temperatura'] = datos['temperatura'].astype(float)
+   datos['humedad'] = datos['humedad'].astype(float)
+    
+    # Filtrar los datos según el rango de fechas
+   datos_filtrados = datos[(datos['dia'] == fecha_seleccionada)]
+    
+    # Calcular el promedio de temperatura en el rango de fechas
+   promedio_temperatura = datos_filtrados['temperatura'].mean()
+   promedio_humedad = datos_filtrados['humedad'].mean()
+    # Crear la figura del gráfico
+   fig = go.Figure()
+   fig1 = go.Figure() 
+    # Añadir la línea de temperaturas
+   fig.add_trace(go.Scatter(x=datos_filtrados['Hora'], y=datos_filtrados['temperatura'],mode='lines+markers', line=dict(color='red'),name='Temperatura'))
+   fig.update_layout(
+            title=f"Temperatura Promedio de {fecha_seleccionada}",
+            xaxis_title="Fecha",
+            yaxis_title="Temperatura (°C)",
+            height=400,
+            xaxis_tickformat='%Y-%m-%d',
+            xaxis=dict(tickmode='array', tickvals=datos_filtrados['Hora'])
+         ) 
+    # Añadir la línea del promedio
+   fig.add_trace(go.Scatter(x=datos_filtrados['Hora'], y=[promedio_temperatura] * len(datos_filtrados),
+                           mode='lines', line=dict(dash='dash', color='cyan'),
+                           name=f"Promedio: {promedio_temperatura:.2f} °C"))
+    
+   
+    # Añadir la línea del promedio
+   fig1.add_trace(go.Scatter(x=datos_filtrados['Hora'], y=[promedio_humedad] * len(datos_filtrados),
+                           mode='lines', line=dict(dash='dash', color='Red'),
+                           name=f"Promedio: {promedio_humedad:.2f} %"))
+    
+    # Añadir título y etiquetas
+   fig1.update_layout(
+      title=f"Humedad promedio de {fecha_seleccionada}",
+      xaxis_title="Fecha",
+      yaxis_title="Humedad %"
+   )
+    # Mostrar el gráfico en Streamlit
+   st.plotly_chart(fig)
+   st.plotly_chart(fig1)
+else:
+   if fecha_seleccionada :
+       try:
+         
+           fecha_seleccionada = pd.to_datetime(fecha_seleccionada).date()
+
+              
+           
+              # Convertir la columna 'dia' a datetime para poder filtrar
+           datos['dia'] = pd.to_datetime(datos['dia']).dt.date
+          
+           datos['temperatura'] = datos['temperatura'].astype(float)   
+          
+           datos_filtrados = datos[(datos['dia'] == fecha_seleccionada)]
+          
+           promedio_temperatura = datos_filtrados['temperatura'].mean() 
+             
+              # Gráfico de Temperatura
+           fig_temp = go.Figure()
+           fig_temp.add_trace(go.Scatter(x=datos_filtrados['Hora'], y=datos_filtrados['temperatura'], mode='lines', name='Temperatura', line=dict(color='red')))
+           fig_temp.update_layout(
+               title=f"Temperatura de {fecha_seleccionada}",
+               xaxis_title="Fecha",
+               yaxis_title="Temperatura (°C)",
+               height=350,
+               xaxis_tickformat='%Y-%m-%d',
+               xaxis=dict(tickmode='array', tickvals=datos_filtrados['Hora'])
+           )
+      
+              # Gráfico de Humedad
+           fig_humedad = go.Figure()
+           fig_humedad.add_trace(go.Scatter(x=datos_filtrados['Hora'], y=datos_filtrados['humedad'], mode='lines', name='Humedad', line=dict(color='blue')))
+           fig_humedad.update_layout(
+               title=f"Humedad de {fecha_seleccionada}",
+               xaxis_title="Fecha",
+               yaxis_title="Humedad (%)",
+               height=350,
+               xaxis_tickformat='%Y-%m-%d',
+               xaxis=dict(tickmode='array', tickvals=datos_filtrados['Hora'])
+           )
+      
+              # Mostrar los gráficos en Streamlit
+           st.plotly_chart(fig_temp)
+           st.plotly_chart(fig_humedad)
+       except Exception as e:
+           st.error(f"Error al procesar el rango de fechas: {e}")
